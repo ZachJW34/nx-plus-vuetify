dee46926a016f445dc659d225f355b933c1ab93b
Zachary Williams <zachjw34@gmail.com>
2020-08-17 20:53:20 -0500

    add vuetify

--- apps/my-app/src/App.vue
+++ apps/my-app/src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-app>
+    <div id="app">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    </div>
+  </v-app>
 </template>
 
 <script lang="ts">
