0a130c35e186871d5b9a4de5b05ba99d7ee3fcfc
Zachary Williams <zachjw34@gmail.com>
2020-08-17 20:35:41 -0500

    add vue application

---
+++ apps/my-app/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'my-app',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/apps/my-app',
+  snapshotSerializers: ['jest-serializer-vue'],
+};
